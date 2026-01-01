90cfda3f6201e1eaa71baecd3d3fdbb167ad4966
Lewi Meyvianus Borosi <lewiyeeey@gmail.com>
2026-01-01 10:09:38 +0700

    feat: setting up routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,19 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  prefix,
+  route,
+} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"),
+  route("login", "./routes/login.tsx"),
+
+  ...prefix("auth", [
+    layout("./routes/auth/layout.tsx", [
+      index("./routes/auth/dashboard.tsx"),
+      route("/transactions", "./routes/auth/transactions.tsx"),
+    ]),
+  ]),
+] satisfies RouteConfig;
