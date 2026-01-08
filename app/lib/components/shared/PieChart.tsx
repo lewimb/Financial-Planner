0c78190f4cef3087b61c862393f85a5d1ffa285a
Lewi Meyvianus Borosi <lewiyeeey@gmail.com>
2026-01-08 11:11:47 +0700

    feat: add reusable pie chart component

---
+++ app/lib/components/shared/PieChart.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import { TrendingUp } from "lucide-react";
+import { Pie, PieChart, Sector } from "recharts";
+import type { PieSectorDataItem } from "recharts/types/polar/Pie";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+  type ChartConfig,
+} from "~/components/ui/chart";
+import { cn } from "~/lib/utils";
+
+export const description = "A donut chart with an active sector";
+
+const chartConfig = {
+  spending: {
+    label: "spending",
+  },
+  chrome: {
+    label: "Chrome",
+    color: "var(--chart-1)",
+  },
+  safari: {
+    label: "Safari",
+    color: "var(--chart-2)",
+  },
+  firefox: {
+    label: "Firefox",
+    color: "var(--chart-3)",
+  },
+  edge: {
+    label: "Edge",
+    color: "var(--chart-4)",
+  },
+  other: {
+    label: "Other",
+    color: "var(--chart-5)",
+  },
+} satisfies ChartConfig;
+
+interface ChartItem {
+  category: string;
+  spending: number;
+  fill: string;
+}
+
+interface PieProps {
+  title: string;
+  description?: string;
+  className?: string;
+  children?: React.ReactNode;
+  chartData: ChartItem[];
+}
+
+export function ChartPieDonutActive({
+  title,
+  description,
+  className,
+  children,
+  chartData,
+}: PieProps) {
+  return (
+    <Card className="flex flex-col">
+      <CardHeader className="items-center pb-0">
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className={cn("mx-auto aspect-square max-h-[250px]", className)}
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Pie
+              data={chartData}
+              dataKey="spending"
+              nameKey="category"
+              innerRadius={60}
+              strokeWidth={5}
+              activeIndex={0}
+              activeShape={({
+                outerRadius = 0,
+                ...props
+              }: PieSectorDataItem) => (
+                <Sector {...props} outerRadius={outerRadius + 10} />
+              )}
+            />
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col gap-2 text-sm">{children}</CardFooter>
+    </Card>
+  );
+}
