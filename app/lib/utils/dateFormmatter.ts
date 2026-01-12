8ed8001e8b19b4b20639fb5ea691f2c97c25acc6
Lewi Meyvianus Borosi <lewiyeeey@gmail.com>
2026-01-12 19:55:42 +0700

    feat: add remaining date function

--- app/lib/utils/dateFormmatter.ts
+++ app/lib/utils/dateFormmatter.ts
@@ -1,9 +1,24 @@
+export function getCurrentDate() {
+  return new Date();
+}
+
 export function formatDate(dateStr: string): string {
   const date = new Date(dateStr);
 
-  return date.toLocaleDateString("en-US", {
+  return date.toLocaleDateString("id-ID", {
     month: "short",
     day: "numeric",
     year: "numeric",
   });
+}
+
+export function remainingDate(deadline: Date) {
+  const currentDate = getCurrentDate();
+  const yearDiff = deadline.getFullYear() - currentDate.getFullYear();
+  const monthDiff = deadline.getMonth() - currentDate.getMonth();
+  const dateDiff = deadline.getDate() - currentDate.getDate();
+
+  const totalDateDiff = yearDiff * 12 * 30 + monthDiff * 30 + dateDiff;
+
+  return totalDateDiff;
 }
