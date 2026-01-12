05a3f3eec4c61b3a8fd34073e8bc10597da083ae
Lewi Meyvianus Borosi <lewiyeeey@gmail.com>
2026-01-12 19:55:56 +0700

    feat: add entrypoint for goals page

--- app/routes/auth/goals.tsx
+++ app/routes/auth/goals.tsx
@@ -1,3 +1,26 @@
+import Header from "~/lib/components/shared/Header";
+import { Modal } from "~/lib/components/shared/Modal";
+import GoalsOverview from "../../lib/components/section/goals/GoalsOverview";
+import GoalsList from "~/lib/components/section/goals/GoalsList";
+import GoalsMilestone from "../../lib/components/section/goals/GoalsMilestone";
+import { GoalsFields } from "~/lib/components/section/goals/GoalsForm";
+
 export default function Goals() {
-  return <div></div>;
+  return (
+    <div className="space-y-6">
+      <Header
+        title="Financial Goals"
+        subtitle="Track progress toward your savings targets"
+      >
+        <Modal label="+ New Goal">
+          <GoalsFields />
+        </Modal>
+      </Header>
+      <GoalsOverview />
+      <div className="grid grid-cols-6 gap-6">
+        <GoalsList />
+        <GoalsMilestone />
+      </div>
+    </div>
+  );
 }
