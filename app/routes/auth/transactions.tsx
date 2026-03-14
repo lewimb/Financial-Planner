4b768409400e7e14a2143253b5cbf5394c6787a6
Lewi Meyvianus Borosi <lewiyeeey@gmail.com>
2026-03-14 11:14:21 +0700

    feat: integrate transactions with api

--- app/routes/auth/transactions.tsx
+++ app/routes/auth/transactions.tsx
@@ -1,9 +1,37 @@
 import Header from "~/lib/components/shared/Header";
 import TransactionOverview from "../../lib/components/section/transaction/TransactionOverview";
 import TransactionTable from "~/lib/components/section/transaction/TransactionTable";
-import TransactionForm from "~/lib/components/section/transaction/TransactionForm";
+import TransactionFormTab from "~/lib/components/section/transaction/TransactionFormTab";
 import { Modal } from "~/lib/components/shared/Modal";
-export default function Transaction() {
+import tokenParser from "~/lib/utils/tokenParser";
+import type { Route } from "./+types/transactions";
+import { useGetTransactionById } from "~/hooks/transactions/use-transaction";
+import Loading from "~/lib/components/shared/Loading";
+import { addToken } from "~/features/users/userSlice";
+import { addUser } from "~/features/users/userSlice";
+import { useAppDispatch } from "~/hooks";
+import type { Transaction } from "~/lib/types/transaction";
+
+export function loader({ request }: Route.LoaderArgs) {
+  return tokenParser(request);
+}
+
+export default function Transaction({ loaderData }: Route.ComponentProps) {
+  const dispatch = useAppDispatch();
+  const { data, isLoading } = useGetTransactionById(
+    loaderData?.token,
+    loaderData?.payload.userId,
+  );
+
+  if (loaderData?.token) {
+    dispatch(addToken(loaderData?.token));
+    dispatch(addUser(loaderData?.payload));
+  }
+
+  if (isLoading || !data) return <Loading />;
+
+  console.log(data);
+
   return (
     <section className="space-y-6">
       <Header
@@ -11,11 +39,11 @@ export default function Transaction() {
         subtitle="Track and manage your income and expenses"
       >
         <Modal label="+ Add Transaction">
-          <TransactionForm />
+          <TransactionFormTab />
         </Modal>
       </Header>
-      <TransactionOverview />
-      <TransactionTable />
+      <TransactionOverview items={data?.items} />
+      <TransactionTable totalData={data?.totalData} items={data?.items} />
     </section>
   );
 }
