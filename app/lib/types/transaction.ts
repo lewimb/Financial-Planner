7b5713b6e2681b2bfdc0e75ba6be9e5f3043dea6
Lewi Meyvianus Borosi <lewiyeeey@gmail.com>
2026-03-14 11:11:59 +0700

    feat: add auth,response,transaction,user typing

---
+++ app/lib/types/transaction.ts
@@ -0,0 +1,34 @@
+import z from "zod";
+
+export interface Transaction {
+  id: number;
+  userId: number;
+  amount: number;
+  transactionTypes: string;
+  description: string;
+  account: string;
+  date: Date;
+  updatedAt: Date;
+  createdAt: Date;
+}
+
+export interface TransactionForm {
+  userId: number | undefined;
+  amount: number;
+  type: string;
+  description: string;
+  account: string;
+  category: string;
+  date: Date;
+}
+
+export const formSchema = z.object({
+  amount: z.string().min(1, {
+    message: "Amount is required",
+  }),
+  description: z.string().min(1, { message: "Description is required" }),
+  category: z.string().min(1, { message: "Category is required" }),
+  date: z.date().min(1, { message: "Date is required" }),
+  account: z.string().min(1, { message: "Account is required" }),
+  type: z.string().min(1, { message: "Transaction types is required" }),
+});
