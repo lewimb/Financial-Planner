16d5da6b8d724eff62eb4433264d664ca7a37100
Lewi Meyvianus Borosi <lewiyeeey@gmail.com>
2026-03-14 11:03:25 +0700

    update: update column for transactions

--- app/lib/components/section/transaction/TransactionColumns.tsx
+++ app/lib/components/section/transaction/TransactionColumns.tsx
@@ -2,15 +2,14 @@ import type { ColumnDef } from "@tanstack/react-table";
 import { formatRupiah } from "../../../utils/currencyFormatter";
 import { ArrowUpRight } from "lucide-react";
 import { cn } from "~/lib/utils";
+import type { Transaction } from "~/lib/types/transaction";
+import { Button } from "~/components/ui/button";
 
-export interface Transaction {
-  id: number;
-  description: string;
-  category: string;
-  date: string;
-  account: string;
-  amount: number;
-  type: "income" | "expense";
+function capitalizeFirstWord(value: string) {
+  const splittedString = value.split(" ");
+  return splittedString
+    .map((val) => val.charAt(0).toUpperCase() + val.slice(1))
+    .join(" ");
 }
 
 export const columns: ColumnDef<Transaction>[] = [
@@ -22,13 +21,14 @@ export const columns: ColumnDef<Transaction>[] = [
         <div
           className={cn(
             "p-3 rounded-full bg-green-200 text-green-600",
-            row.getValue("type") === "expense" && "bg-red-200 text-red-600"
+            row.getValue("transactionTypes") === "expense" &&
+              "bg-red-200 text-red-600",
           )}
         >
           <ArrowUpRight
             className={cn(
               "size-4",
-              row.getValue("type") === "expense" && "rotate-z-90"
+              row.getValue("transactionTypes") === "expense" && "rotate-z-90",
             )}
           />
         </div>
@@ -62,25 +62,29 @@ export const columns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "account",
     header: "Account",
-    cell: ({ row }) => <div>{row.getValue("account")}</div>,
+    cell: ({ row }) => (
+      <div>{capitalizeFirstWord(row.getValue("account"))}</div>
+    ),
   },
   {
-    accessorKey: "type",
-    header: "Type",
+    accessorKey: "transactionTypes",
+    header: "transactionTypes",
     cell: ({ row }) => {
-      const type = row.getValue<"income" | "expense">("type");
+      const transactionTypes = row.getValue<"income" | "expense">(
+        "transactionTypes",
+      );
 
       return (
         <span
           className={`rounded-full px-2 py-1 text-xs capitalize font-medium
             ${
-              type === "income"
+              transactionTypes === "income"
                 ? "bg-green-100 text-green-700"
                 : "bg-red-100 text-red-700"
             }
           `}
         >
-          {type}
+          {transactionTypes}
         </span>
       );
     },
@@ -90,14 +94,34 @@ export const columns: ColumnDef<Transaction>[] = [
     header: () => <div className="text-right">Amount</div>,
     cell: ({ row }) => {
       const amount = row.getValue<number>("amount");
+      const isIncome = row.original.transactionTypes === "income";
 
       return (
-        <div
-          className={`text-right font-medium ${
-            row.original.type === "income" ? "text-green-600" : "text-red-600"
-          }`}
-        >
-          {formatRupiah(amount)}
+        <div className="relative flex items-center justify-end overflow-hidden py-4">
+          <div
+            className={cn(
+              "text-right font-medium transition-all duration-300 group-hover:-translate-x-10 group-hover:opacity-0",
+              isIncome ? "text-green-600" : "text-red-600",
+            )}
+          >
+            {formatRupiah(amount)}
+          </div>
+
+          <div className="absolute right-0 flex translate-x-full items-center gap-2 transition-transform duration-300 group-hover:translate-x-0 ">
+            <Button
+              variant="outline"
+              onClick={() => console.log("Edit", row.original)}
+            >
+              Edit
+            </Button>
+
+            <Button
+              variant="destructive"
+              onClick={() => console.log("Delete", row.original)}
+            >
+              Delete
+            </Button>
+          </div>
         </div>
       );
     },
