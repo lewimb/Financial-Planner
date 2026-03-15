7ed634aeff1db7d96af7070c48dccdabaeccc06c
Lewi Meyvianus Borosi <lewiyeeey@gmail.com>
2026-03-15 08:41:53 +0700

    feat: add delete and update method to transactions

--- app/hooks/transactions/use-transaction.ts
+++ app/hooks/transactions/use-transaction.ts
@@ -1,5 +1,10 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import type { TransactionForm } from "~/lib/types/transaction";
+import { toast } from "sonner";
+
+interface Props {
+  id: string | undefined;
+}
 
 export function useGetTransactionById(
   token: string | undefined,
@@ -46,6 +51,57 @@ export function useCreateTransaction({ token }: { token: string }) {
       }),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["transactions"] });
+    },
+  });
+}
+
+export function useDeleteTransaction(token: string | undefined) {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (id: string) =>
+      fetch(`http://localhost:8080/api/v1/transaction/${id}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["transactions"] });
+      toast.success("Transaction deleted successfully", {
+        position: "top-right",
+      });
+    },
+    onError: () => {
+      toast.error("Failed to delete transaction", { position: "top-right" });
+    },
+  });
+}
+
+export function useUpdateTransaction(
+  token: string | undefined,
+  userId: number | undefined,
+) {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (value: TransactionForm) =>
+      fetch(`http://localhost:8080/api/v1/transaction/${userId}`, {
+        method: "PUT",
+        body: JSON.stringify(value),
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-type": "application/json",
+        },
+      }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["transactions"] });
+      toast.success("Transaction updated successfully", {
+        position: "top-right",
+      });
+    },
+    onError: () => {
+      toast.error("Failed to update transaction", { position: "top-right" });
     },
   });
 }
